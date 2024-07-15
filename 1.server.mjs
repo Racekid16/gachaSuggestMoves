79375f79ecdc54db74ab6e8d6c9613f8b8cb9315
Racekid16 <1750801177a@gmail.com>
2024-07-15 14:06:06 -0400

    More server code reorganization
    Moved some utility functions to a new file.
    Imported consts and config like I did in the driver files, since I now can with the ES6 module import syntax.

--- 1.server.mjs
+++ 1.server.mjs
@@ -10,17 +10,16 @@
 
 import express from 'express';
 import cors from 'cors';
+import { fileURLToPath } from 'url';
 import path from 'path';
-import { connectToDb, getDb } from './serverModules/database.mjs';
-import fs from 'fs';
 import getPixels from 'get-pixels';
+import { connectToDb, getDb } from './serverModules/database.mjs';
+import { getSlot, slotDifference, getNumStars, getAspect } from './serverModules/parseImage.mjs';
+import consts from './consts.json' assert { type: 'json' };
+import config from './config.json' assert { type: 'json' };
 
 const server = express();
 server.use(express.json());
-
-const constsFile = './consts.json';
-const configFile = './config.json';
-const imageWidth = 328;
 
 //allow requests to be made to this server from any url
 server.use(cors({
@@ -33,9 +32,7 @@ let database;
 connectToDb((error) => {
     if (!error) {
         //start server
-        let consts = fs.readFileSync(constsFile, 'utf-8');
-        let constsJSON = JSON.parse(consts);
-        const port = constsJSON.port;
+        const port = consts.port;
         server.listen(port, '127.0.0.1', () => {
             console.log(`Listening on port ${port}`);
         });
@@ -44,6 +41,8 @@ connectToDb((error) => {
 })
 
 server.get("/", (req, res) => {
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = path.dirname(__filename);
     res.sendFile(path.join(__dirname, 'index.html'));
 })
 
@@ -279,7 +278,7 @@ server.post("/ImageData/updateDb", (req, res) => {
         return;
     }
     getPixels(req.body.imageURL, function(err, pixels) {
-        if(err) {
+        if (err) {
             console.log(`Bad image path (2): ${req.body.imageURL}`);
             res.status(400).send({
                 message: "Invalid imageURL"
@@ -385,207 +384,8 @@ server.post("/ImageData/parseParty", (req, res) => {
     });
 })
 
-function pixelArrayIndex(pixelX, pixelY) {
-    return 4 * (imageWidth * pixelY + pixelX);
-}
-
-function getSlot(minX, minY, maxX, maxY, slotName, image) {
-    let slot = {
-        name: slotName,
-        pixels: []
-    };
-    for (let i = minY; i <= maxY; i++) {
-        let rowStartIndex = pixelArrayIndex(minX, i);
-        let rowEndIndex = pixelArrayIndex(maxX, i) + 3;
-        for (let j = rowStartIndex; j <= rowEndIndex; j++) {
-            slot.pixels.push(image.pixels[j]);
-        }
-    }
-    return slot;
-}
-
-function slotDifference(slot1, slot2) {
-    if (slot1.pixels.length != slot2.pixels.length) {
-        console.log(`Error: ${slot1.name} is ${slot1.pixels.length / 4} pixels but ${slot2.name} is ${slot2.pixels.length / 4} pixels.`);
-        return;
-    }
-    let difference = 0;
-    for (let i = 0; i < slot1.pixels.length; ++i) {
-        difference += Math.abs(slot1.pixels[i] - slot2.pixels[i]);
-    }
-    return difference;
-}
-
-function RGBA_at(pixelX, pixelY, image) {
-    let startIndex = pixelArrayIndex(pixelX, pixelY);
-    return [image.pixels[startIndex], image.pixels[startIndex + 1], image.pixels[startIndex + 2], image.pixels[startIndex + 3]];
-}
-
-function RGBA_difference(pixel1, pixel2) {
-    let difference = 0;
-    for (let i = 0; i < 4; i++) {
-        difference += Math.abs(pixel1[i] - pixel2[i]);
-    }
-    return difference;
-}
-
-function getNumStars(slotName, image) {
-    //pixel values are chosen according to 4 star sato
-    //except for the fifth star, which is chosen according to 5 star kei
-    switch (slotName) {
-
-        case 'slot1':
-            if (RGBA_difference(RGBA_at(81, 92, image), [244, 130, 124, 255]) > 55) {
-                return 1;
-            }
-            if (RGBA_difference(RGBA_at(75, 92, image), [213, 80 , 123, 255]) > 55) {
-                return 2;
-            }
-            if (RGBA_difference(RGBA_at(65, 93, image), [243, 141, 136, 255]) > 55) {
-                return 3;
-            }
-            if (RGBA_difference(RGBA_at(59, 94, image), [219, 103, 104, 255]) > 55) {
-                return 4;
-            }
-            return 5;
-
-        case 'slot2':
-            if (RGBA_difference(RGBA_at(184, 92, image), [242, 127, 122, 255]) > 55) {
-                return 1;
-            }
-            if (RGBA_difference(RGBA_at(176, 93, image), [235, 116, 111, 255]) > 55) {
-                return 2;
-            }
-            if (RGBA_difference(RGBA_at(168, 94, image), [237, 114, 121, 255]) > 55) {
-                return 3;
-            }
-            if (RGBA_difference(RGBA_at(162, 95, image), [234, 119, 115, 255]) > 55) {
-                return 4;
-            }
-            return 5;
-
-        case 'slot3':
-            if (RGBA_difference(RGBA_at(286, 92, image), [246, 121, 121, 255]) > 55) {
-                return 1;
-            }
-            if (RGBA_difference(RGBA_at(279, 93, image), [235, 117, 111, 255]) > 55) {
-                return 2;
-            }
-            if (RGBA_difference(RGBA_at(270, 94, image), [248, 139, 135, 255]) > 55) {
-                return 3;
-            }
-            if (RGBA_difference(RGBA_at(265, 95, image), [235, 120, 115, 255]) > 55) {
-                return 4;
-            }
-            return 5;
-
-        case 'slot4':
-            if (RGBA_difference(RGBA_at(81, 210, image), [244, 123, 121, 255]) > 55) {
-                return 1;
-            }
-            if (RGBA_difference(RGBA_at(75, 210, image), [227, 101, 130, 255]) > 55) {
-                return 2;
-            }
-            if (RGBA_difference(RGBA_at(65, 211, image), [246, 154, 144, 255]) > 55) {
-                return 3;
-            }
-            if (RGBA_difference(RGBA_at(59, 213, image), [231, 115, 108, 255]) > 55) {
-                return 4;
-            }
-            return 5;
-
-        case 'slot5':
-            if (RGBA_difference(RGBA_at(184, 212, image), [240, 114, 106, 255]) > 55) {
-                return 1;
-            }
-            if (RGBA_difference(RGBA_at(175, 210, image), [246, 153, 133, 255]) > 55) {
-                return 2;
-            }
-            if (RGBA_difference(RGBA_at(167, 212, image), [248, 128, 129, 255]) > 55) {
-                return 3;
-            }
-            if (RGBA_difference(RGBA_at(162, 213, image), [227, 106, 109, 255]) > 55) {
-                return 4;
-            }
-            return 5;
-
-        case 'slot6':
-            if (RGBA_difference(RGBA_at(287, 212, image), [240, 113, 106, 255]) > 55) {
-                return 1;
-            }
-            if (RGBA_difference(RGBA_at(281, 210, image), [225, 97 , 128, 255]) > 55) {
-                return 2;
-            }
-            if (RGBA_difference(RGBA_at(270, 212, image), [248, 128, 129, 255]) > 55) {
-                return 3;
-            }
-            if (RGBA_difference(RGBA_at(265, 213, image), [227, 107, 109, 255]) > 55) {
-                return 4;
-            }
-            return 5;
-    }
-}
-
-function getAspect(slotName, image) {
-    if (isInfernal(slotName, image)) {
-        return "Infernal ";
-    }
-    if (isTitanium(slotName, image)) {
-        return "Titanium ";
-    }
-    if (isTidal(slotName, image)) {
-        return "Tidal ";
-    }
-    return "";
-}
-
-function isInfernal(slotName, image) {
-    switch (slotName) {
-        case 'slot1':
-            return RGBA_difference(RGBA_at(20, 45, image), [237, 27, 36, 255]) == 0;
-        case 'slot2':
-            return RGBA_difference(RGBA_at(123, 45, image), [237, 27, 36, 255]) == 0;
-        case 'slot3':
-            return RGBA_difference(RGBA_at(226, 45, image), [237, 27, 36, 255]) == 0; 
-        case 'slot4':
-        case 'slot5':
-        case 'slot6':
-            return false;
-    }
-}
-
-function isTitanium(slotName, image) {
-    switch (slotName) {
-        case 'slot1':
-            return RGBA_difference(RGBA_at(20, 45, image), [255, 255, 255, 255]) == 0;
-        case 'slot2':
-            return RGBA_difference(RGBA_at(123, 45, image), [255, 255, 255, 255]) == 0;
-        case 'slot3':
-            return RGBA_difference(RGBA_at(226, 45, image), [255, 255, 255, 255]) == 0; 
-        case 'slot4':
-        case 'slot5':
-        case 'slot6':
-            return false;
-    }
-}
-
-//implement this once there are tidal campaign enemiees
-function isTidal(slotName, image) {
-    switch (slotName) {
-        case 'slot1':
-        case 'slot2':
-        case 'slot3':
-        case 'slot4':
-        case 'slot5':
-        case 'slot6':
-            return false;
-    }
-}
-
 server.get("/getToken", (req, res) => {
-    let config = fs.readFileSync(configFile, 'utf-8');
-    let configJSON = JSON.parse(config);
-    let token = configJSON.token;
+    let token = config.token;
     if (!token) {
         res.status(503).send({
             message: "error retrieving token from server"
---
+++ serverModules/parseImage.mjs
@@ -0,0 +1,199 @@
+//functions for helping with analyzing party images
+
+export function getSlot(minX, minY, maxX, maxY, slotName, image) {
+    let slot = {
+        name: slotName,
+        pixels: []
+    };
+    for (let i = minY; i <= maxY; i++) {
+        let rowStartIndex = pixelArrayIndex(minX, i);
+        let rowEndIndex = pixelArrayIndex(maxX, i) + 3;
+        for (let j = rowStartIndex; j <= rowEndIndex; j++) {
+            slot.pixels.push(image.pixels[j]);
+        }
+    }
+    return slot;
+}
+
+export function slotDifference(slot1, slot2) {
+    if (slot1.pixels.length != slot2.pixels.length) {
+        console.log(`Error: ${slot1.name} is ${slot1.pixels.length / 4} pixels but ${slot2.name} is ${slot2.pixels.length / 4} pixels.`);
+        return;
+    }
+    let difference = 0;
+    for (let i = 0; i < slot1.pixels.length; ++i) {
+        difference += Math.abs(slot1.pixels[i] - slot2.pixels[i]);
+    }
+    return difference;
+}
+
+export function getNumStars(slotName, image) {
+    //pixel values are chosen according to 4 star sato
+    //except for the fifth star, which is chosen according to 5 star kei
+    switch (slotName) {
+
+        case 'slot1':
+            if (RGBA_difference(RGBA_at(81, 92, image), [244, 130, 124, 255]) > 55) {
+                return 1;
+            }
+            if (RGBA_difference(RGBA_at(75, 92, image), [213, 80 , 123, 255]) > 55) {
+                return 2;
+            }
+            if (RGBA_difference(RGBA_at(65, 93, image), [243, 141, 136, 255]) > 55) {
+                return 3;
+            }
+            if (RGBA_difference(RGBA_at(59, 94, image), [219, 103, 104, 255]) > 55) {
+                return 4;
+            }
+            return 5;
+
+        case 'slot2':
+            if (RGBA_difference(RGBA_at(184, 92, image), [242, 127, 122, 255]) > 55) {
+                return 1;
+            }
+            if (RGBA_difference(RGBA_at(176, 93, image), [235, 116, 111, 255]) > 55) {
+                return 2;
+            }
+            if (RGBA_difference(RGBA_at(168, 94, image), [237, 114, 121, 255]) > 55) {
+                return 3;
+            }
+            if (RGBA_difference(RGBA_at(162, 95, image), [234, 119, 115, 255]) > 55) {
+                return 4;
+            }
+            return 5;
+
+        case 'slot3':
+            if (RGBA_difference(RGBA_at(286, 92, image), [246, 121, 121, 255]) > 55) {
+                return 1;
+            }
+            if (RGBA_difference(RGBA_at(279, 93, image), [235, 117, 111, 255]) > 55) {
+                return 2;
+            }
+            if (RGBA_difference(RGBA_at(270, 94, image), [248, 139, 135, 255]) > 55) {
+                return 3;
+            }
+            if (RGBA_difference(RGBA_at(265, 95, image), [235, 120, 115, 255]) > 55) {
+                return 4;
+            }
+            return 5;
+
+        case 'slot4':
+            if (RGBA_difference(RGBA_at(81, 210, image), [244, 123, 121, 255]) > 55) {
+                return 1;
+            }
+            if (RGBA_difference(RGBA_at(75, 210, image), [227, 101, 130, 255]) > 55) {
+                return 2;
+            }
+            if (RGBA_difference(RGBA_at(65, 211, image), [246, 154, 144, 255]) > 55) {
+                return 3;
+            }
+            if (RGBA_difference(RGBA_at(59, 213, image), [231, 115, 108, 255]) > 55) {
+                return 4;
+            }
+            return 5;
+
+        case 'slot5':
+            if (RGBA_difference(RGBA_at(184, 212, image), [240, 114, 106, 255]) > 55) {
+                return 1;
+            }
+            if (RGBA_difference(RGBA_at(175, 210, image), [246, 153, 133, 255]) > 55) {
+                return 2;
+            }
+            if (RGBA_difference(RGBA_at(167, 212, image), [248, 128, 129, 255]) > 55) {
+                return 3;
+            }
+            if (RGBA_difference(RGBA_at(162, 213, image), [227, 106, 109, 255]) > 55) {
+                return 4;
+            }
+            return 5;
+
+        case 'slot6':
+            if (RGBA_difference(RGBA_at(287, 212, image), [240, 113, 106, 255]) > 55) {
+                return 1;
+            }
+            if (RGBA_difference(RGBA_at(281, 210, image), [225, 97 , 128, 255]) > 55) {
+                return 2;
+            }
+            if (RGBA_difference(RGBA_at(270, 212, image), [248, 128, 129, 255]) > 55) {
+                return 3;
+            }
+            if (RGBA_difference(RGBA_at(265, 213, image), [227, 107, 109, 255]) > 55) {
+                return 4;
+            }
+            return 5;
+    }
+}
+
+export function getAspect(slotName, image) {
+    if (isInfernal(slotName, image)) {
+        return "Infernal ";
+    }
+    if (isTitanium(slotName, image)) {
+        return "Titanium ";
+    }
+    if (isTidal(slotName, image)) {
+        return "Tidal ";
+    }
+    return "";
+}
+
+function pixelArrayIndex(pixelX, pixelY) {
+    const imageWidth = 328;
+    return 4 * (imageWidth * pixelY + pixelX);
+}
+
+function RGBA_at(pixelX, pixelY, image) {
+    let startIndex = pixelArrayIndex(pixelX, pixelY);
+    return [image.pixels[startIndex], image.pixels[startIndex + 1], image.pixels[startIndex + 2], image.pixels[startIndex + 3]];
+}
+
+function RGBA_difference(pixel1, pixel2) {
+    let difference = 0;
+    for (let i = 0; i < 4; i++) {
+        difference += Math.abs(pixel1[i] - pixel2[i]);
+    }
+    return difference;
+}
+
+function isInfernal(slotName, image) {
+    switch (slotName) {
+        case 'slot1':
+            return RGBA_difference(RGBA_at(20, 45, image), [237, 27, 36, 255]) == 0;
+        case 'slot2':
+            return RGBA_difference(RGBA_at(123, 45, image), [237, 27, 36, 255]) == 0;
+        case 'slot3':
+            return RGBA_difference(RGBA_at(226, 45, image), [237, 27, 36, 255]) == 0; 
+        case 'slot4':
+        case 'slot5':
+        case 'slot6':
+            return false;
+    }
+}
+
+function isTitanium(slotName, image) {
+    switch (slotName) {
+        case 'slot1':
+            return RGBA_difference(RGBA_at(20, 45, image), [255, 255, 255, 255]) == 0;
+        case 'slot2':
+            return RGBA_difference(RGBA_at(123, 45, image), [255, 255, 255, 255]) == 0;
+        case 'slot3':
+            return RGBA_difference(RGBA_at(226, 45, image), [255, 255, 255, 255]) == 0; 
+        case 'slot4':
+        case 'slot5':
+        case 'slot6':
+            return false;
+    }
+}
+
+//implement this once there are tidal campaign enemiees
+function isTidal(slotName, image) {
+    switch (slotName) {
+        case 'slot1':
+        case 'slot2':
+        case 'slot3':
+        case 'slot4':
+        case 'slot5':
+        case 'slot6':
+            return false;
+    }
+}
